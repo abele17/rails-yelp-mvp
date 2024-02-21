0c65a8f4cd09b4aae711a0da346dbbb6635de066
Pierre Urban <pierreurban1@gmail.com>
2024-02-21 15:14:09 +0100

    rails yelp done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants do
+    # /restaurants/:id/reviews/new
+    resources :reviews
+  end
 end
